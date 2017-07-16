36a1ec84c907427ffb285d497caf60141ec6220c
kimgoon <kimgoon@github.com>
2017-07-16 15:53:40 -0400

    add link to go to history

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -46,12 +46,13 @@ class Game extends React.Component {
         this.state = {
             history: [{squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber+1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -65,13 +66,28 @@ class Game extends React.Component {
                 squares: squares,
             }]),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
         });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext : (step % 2) === 0,
+        })
     }
 
   render() {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+          const desc = move ? "Move #" + move : "Game start";
+          return (
+              <li key={move}><a href="#" onClick={() => this.jumpTo(move)}>{desc}</a></li>
+          );
+      });
 
       let status;
       if (winner) {
@@ -88,7 +104,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
